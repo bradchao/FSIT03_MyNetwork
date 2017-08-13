0b64855167992e6d4f8ee3a423fdd8de523c7731
Chien <chien@gmail.com>
2017-08-13 16:16:52 +0800

    v3

--- app/src/main/java/tw/brad/app/helloworld/mynetwork/MainActivity.java
+++ app/src/main/java/tw/brad/app/helloworld/mynetwork/MainActivity.java
@@ -1,5 +1,9 @@
 package tw.brad.app.helloworld.mynetwork;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,12 +19,15 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
     private ImageView img;
+    private Bitmap bmp;
+    private UIHandler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        handler = new UIHandler();
         img = (ImageView)findViewById(R.id.img);
     }
     public void test1(View view){
@@ -51,14 +58,24 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 try{
-                    URL url = new URL("");
+                    URL url = new URL("http://www.iii.org.tw/assets/images/information-news/image004.jpg");
                     HttpURLConnection conn = (HttpURLConnection)url.openConnection();
                     conn.connect();
-
+                    bmp = BitmapFactory.decodeStream(conn.getInputStream());
+                    handler.sendEmptyMessage(0);
                 }catch(Exception e){
                     Log.i("brad", e.toString());
                 }
             }
         }.start();
     }
+
+    private class UIHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            img.setImageBitmap(bmp);
+        }
+    }
+
 }
